b158b93c803be39fe9129d39a9ff0dc95f56a1d4
john verzani <jverzani@gmail.com>
2012-09-30 18:26:06 -0400

    have issue with database locking

--- R/db.R
+++ R/db.R
@@ -7,21 +7,24 @@ NULL
 ## we have a global connection ..con.. that gets created in web.R
 
 db_name <- function(dbname) {
-  tmp_dir <- getOption("gWidgetsWWW2.rapache::tmp_dir") %||% "/tmp"
+  tmp_dir <- getOption("gWidgetsWWW2.rapache::tmp_dir") %||% "/tmp/gw"
+  if(!(file.exists(tmp_dir) && file.info(tmp_dir)$isdir))
+    dir.create(tmp_dir, recursive=TRUE)
   sprintf("%s/%s.db", tmp_dir, dbname)
 }
 
 
 open_connection <- function(dbname=..ID..) {
   drv <- dbDriver("SQLite")
-  
+  ## how to check if okay
   f <- db_name(dbname)
   db <- dbConnect(drv, dbname = f)
-
   db
 }
 
-disconnect_connect <- function(db) dbDisconnect(db)
+disconnect_connect <- function(db) {
+  dbDisconnect(db)
+}
 
 create_table <- function(db) {
   
@@ -47,7 +50,7 @@ new_item <- function() {
                           items="",
                           properties=""
                           )
-  sql <- "insert into widgets (value, items, properties) values ('', '{}', '{}');"
+  sql <- "insert OR IGNORE into widgets (value, items, properties) values ('', '{}', '{}');"
   dbGetQuery(db, whisker.render(sql))
 
   sql <- "SELECT last_insert_rowid()"
@@ -67,27 +70,48 @@ set_vals <- function(id, value, items, properties, db=..con..) {
   if(!missing(properties))
     l$properties <- toJSON(properties) 
 
-  dbBeginTransaction(db)
-  mapply(function(key, value,  id) {
-    sql <- whisker.render('update widgets set {{key}} = :value where id=:id')
+  out <- try({
+    dbBeginTransaction(db)
+    tmp <- mapply(function(key, value,  id) {
+      sql <- whisker.render('update OR IGNORE widgets set {{key}} = :value where id=:id')
+      
+      res <- dbGetPreparedQuery(db, sql,
+                                bind.data=data.frame(id=id, value=value,
+                                  stringsAsFactors=FALSE))
+    }, names(l), l, id=id)
 
-    dbGetPreparedQuery(db, sql,
-                       bind.data=data.frame(id=id, value=value,
-                         stringsAsFactors=FALSE))
-    
-  }, names(l), l, id=id)
-  dbCommit(db)
+    dbCommit(db)
+  }, silent=TRUE)
+
+  if(inherits(out, "try-error"))
+    message("Couldn't write to data base")
 }
 
 
 get_vals <- function(id, key=c("value", "items", "properties")) {
   db <- ..con..
-  
   key <- match.arg(key)
 
   sql <- whisker.render('select {{key}} from widgets where id={{id}}')
-  out <- dbGetQuery(db, sql)[,,drop=TRUE]
 
+  ctr <- 1
+  out <- try(dbGetQuery(db, sql), silent=TRUE)
+  if(inherits(out, "try-error")) {
+    message("Couldn't get vals")
+    stop()
+  }
+  ## while(inherits(out, "try-error")) {
+  ##   Sys.sleep(.05)                      # 50ms
+  ##   ctr <- ctr + 1
+  ##   message("get vals, trying ctr=", ctr)
+  ##   if(ctr >= 100) stop("Can't get lock on database file")
+  ##   out <- try(dbGetQuery(db, sql), silent=TRUE)
+  ## }
+
+  
+
+  out <- out[,,drop=TRUE]
+  
   if(key %in% c("items", "properties")) {
     if(nchar(out)) {
       out <- fromJSON(out)
